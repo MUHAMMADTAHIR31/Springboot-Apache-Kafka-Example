5d93d683f753d7c89e5a027f1c9d9f49f16247fd
Muhammad Tahir <tahirsindhi872@gmail.com>
2022-07-25 01:00:47 +0500

    Update

---
+++ src/main/java/com/springboot/kafka/SpringbootApacheKafkaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApacheKafkaExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApacheKafkaExampleApplication.class, args);
+    }
+
+}
