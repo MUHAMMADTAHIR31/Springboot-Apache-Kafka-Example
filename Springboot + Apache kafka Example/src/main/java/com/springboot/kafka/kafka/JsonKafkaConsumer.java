9d3d875880575c8dae3a67f153d88ad1dea9b346
Muhammad Tahir <64077988+MUHAMMADTAHIR31@users.noreply.github.com>
2022-07-26 15:12:07 +0500

    Add files via upload

---
+++ Springboot + Apache kafka Example/src/main/java/com/springboot/kafka/kafka/JsonKafkaConsumer.java
@@ -0,0 +1,17 @@
+package com.springboot.kafka.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonKafkaConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaConsumer.class);
+
+    @KafkaListener(topics = "javaguides_json",groupId = "myGroup")
+    public void consume(String message){
+        LOGGER.info(String.format("Message Received -> %s",message));
+    }
+}
